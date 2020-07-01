b6eb7d599eb52695f99567586424bcc44885c9e4
Repeat Yang <repeat@pixnet.tw>
2020-07-01 17:47:33 +0800

    update composer req script

--- composer-g-req-dev.sh
+++ composer-g-req-dev.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 /usr/bin/env composer g req --dev \
-nesbot/carbon ^2.30 \
-psy/psysh ^0.9.12 \
-phpunit/phpunit ^8.0 \
-phpmd/phpmd ^2.8 \
-squizlabs/php_codesniffer ^3.5 \
-friendsofphp/php-cs-fixer ^2.16 \
-sebastian/phpcpd ^4.1 \
-phploc/phploc ^5.0
-
-cp CodeSniffer.conf ~/.composer/vendor/squizlabs/php_codesniffer
+nesbot/carbon ^2.35 \
+psy/psysh ^0.10 \
+firebase/php-jwt ^5.2
