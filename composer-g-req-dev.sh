518faff656aac4df67c1b6ddbfbdbded3af5e3ad
Repeat Yang <repeat@pixnet.tw>
2020-08-25 11:56:09 +0800

    add phpcs back

--- composer-g-req-dev.sh
+++ composer-g-req-dev.sh
@@ -2,4 +2,7 @@
 /usr/bin/env composer g req --dev \
 nesbot/carbon ^2.35 \
 psy/psysh ^0.10 \
-firebase/php-jwt ^5.2
+firebase/php-jwt ^5.2 \
+squizlabs/php_codesniffer ^3.5
+
+cp CodeSniffer.conf ~/.composer/vendor/squizlabs/php_codesniffer
